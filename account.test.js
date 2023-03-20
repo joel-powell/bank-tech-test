274a2eef729ad55c8cd0bf8e817e32db9ff24dd2
Joel Powell <joelhpowell@icloud.com>
2023-03-20 17:05:33 +0000

    add passing test for decimals and clean up tests

--- account.test.js
+++ account.test.js
@@ -19,7 +19,7 @@ describe("Account", () => {
         account.statement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
-          expect.stringContaining("date || credit || debit || balance")
+          `date || credit || debit || balance`
         );
       });
     });
@@ -33,10 +33,8 @@ describe("Account", () => {
         account.statement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
-          expect.stringContaining("date || credit || debit || balance")
-        );
-        expect(consoleSpy).toHaveBeenCalledWith(
-          expect.stringContaining("10/01/2023 || 1000.00 || || 1000.00")
+          `date || credit || debit || balance
+10/01/2023 || 1000.00 || || 1000.00`
         );
       });
     });
@@ -52,11 +50,9 @@ describe("Account", () => {
         account.statement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
-          [
-            "date || credit || debit || balance",
-            "13/01/2023 || 2000.00 || || 3000.00",
-            "10/01/2023 || 1000.00 || || 1000.00",
-          ].join("\n")
+          `date || credit || debit || balance
+13/01/2023 || 2000.00 || || 3000.00
+10/01/2023 || 1000.00 || || 1000.00`
         );
       });
     });
@@ -70,16 +66,14 @@ describe("Account", () => {
         account.statement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
-          [
-            "date || credit || debit || balance",
-            "14/01/2023 || || 500.00 || -500.00",
-          ].join("\n")
+          `date || credit || debit || balance
+14/01/2023 || || 500.00 || -500.00`
         );
       });
     });
 
     describe("given two deposits and a withdrawal", () => {
-      it("returns the headings and the deposit details with running balance", () => {
+      it("returns the headings and the transaction details with running balance", () => {
         const account = new Account();
 
         jest.setSystemTime(new Date("2023-01-10"));
@@ -91,12 +85,31 @@ describe("Account", () => {
         account.statement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
-          [
-            "date || credit || debit || balance",
-            "14/01/2023 || || 500.00 || 2500.00",
-            "13/01/2023 || 2000.00 || || 3000.00",
-            "10/01/2023 || 1000.00 || || 1000.00",
-          ].join("\n")
+          `date || credit || debit || balance
+14/01/2023 || || 500.00 || 2500.00
+13/01/2023 || 2000.00 || || 3000.00
+10/01/2023 || 1000.00 || || 1000.00`
+        );
+      });
+    });
+
+    describe("given decimal amounts", () => {
+      it("returns the headings and the transaction details with running balance", () => {
+        const account = new Account();
+
+        jest.setSystemTime(new Date("2023-01-10"));
+        account.deposit(10.23);
+        jest.setSystemTime(new Date("2023-01-13"));
+        account.deposit(25.4);
+        jest.setSystemTime(new Date("2023-01-14"));
+        account.withdraw(5.02);
+        account.statement();
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          `date || credit || debit || balance
+14/01/2023 || || 5.02 || 30.61
+13/01/2023 || 25.40 || || 35.63
+10/01/2023 || 10.23 || || 10.23`
         );
       });
     });
