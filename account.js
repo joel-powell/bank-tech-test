d7ce03dd9842d14d79539b774b3a6be26497dacf
Joel Powell <joelhpowell@icloud.com>
2023-03-20 11:55:49 +0000

    add initial test for statement class method

--- account.js
+++ account.js
@@ -1,3 +1,7 @@
 module.exports = class Account {
   transactions = [];
+
+  statement() {
+    return "date || credit || debit || balance";
+  }
 };
--- account.test.js
+++ account.test.js
@@ -3,7 +3,17 @@ const Account = require("./account");
 describe("Account", () => {
   const account = new Account();
 
-  it("initialises", () => {
-    expect(account.transactions).toEqual([]);
+  describe("initially", () => {
+    it("returns an empty array", () => {
+      expect(account.transactions).toEqual([]);
+    });
+  });
+
+  describe("#statement", () => {
+    describe("#initially", () => {
+      it("returns only the headings", () => {
+        expect(account.statement()).toBe("date || credit || debit || balance");
+      });
+    });
   });
 });
