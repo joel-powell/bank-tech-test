79b05d472f92b0138c3b18aa77e166c9b6b8617b
Joel Powell <joelhpowell@icloud.com>
2023-03-20 15:11:45 +0000

    add passing test for two deposits and a withdrawal

--- account.test.js
+++ account.test.js
@@ -77,5 +77,28 @@ describe("Account", () => {
         );
       });
     });
+
+    describe("given two deposits and a withdrawal", () => {
+      it("returns the headings and the deposit details with running balance", () => {
+        const account = new Account();
+
+        jest.setSystemTime(new Date("2023-01-10"));
+        account.deposit(1000);
+        jest.setSystemTime(new Date("2023-01-13"));
+        account.deposit(2000);
+        jest.setSystemTime(new Date("2023-01-14"));
+        account.withdraw(500);
+        account.statement();
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          [
+            "date || credit || debit || balance",
+            "14/01/2023 || || 500.00 || 2500.00",
+            "13/01/2023 || 2000.00 || || 3000.00",
+            "10/01/2023 || 1000.00 || || 1000.00",
+          ].join("\n")
+        );
+      });
+    });
   });
 });
